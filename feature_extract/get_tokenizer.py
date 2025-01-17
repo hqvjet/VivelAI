e0693c59b2fb1773ef7dc45ebe2bc78d00388430
hqvjet <viethq.1906@gmail.com>
2025-01-17 20:51:31 +0700

    init

--- feature_extract/get_tokenizer.py
+++ feature_extract/get_tokenizer.py
@@ -4,8 +4,8 @@ from constant import PHOBERT_VER, EMOJI_NEG, EMOJI_NEU, EMOJI_POS
 def getTokenizer(e_matrix=None):
     tokenizer = AutoTokenizer.from_pretrained(PHOBERT_VER)
     # toks = [EMOJI_NEG, EMOJI_NEU, EMOJI_POS]
-    toks = list(e_matrix.keys())
-
-    num_added = tokenizer.add_tokens(toks)
+    # toks = list(e_matrix.keys())
+    #
+    # num_added = tokenizer.add_tokens(toks)
 
     return tokenizer
