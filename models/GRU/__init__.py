d7e562f66401d628b5a6e55e74ce3f469f89cc00
hqvjet <viethq.1906@gmail.com>
2025-01-18 03:39:41 +0700

    update

--- models/GRU/__init__.py
+++ models/GRU/__init__.py
@@ -7,7 +7,7 @@ with open('models/LSTM/config.json', 'r') as file:
 
 phobert_config = config['phobert']
 phow2v_config = config['phow2v']
-num_classes = 2
+num_classes = 3
 
 class GRU(nn.Module):
     def __init__(self, device, input_shape, emb_tech, dropout=0.1):
