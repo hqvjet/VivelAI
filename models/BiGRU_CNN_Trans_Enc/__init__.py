7c624267e8eb394b3b4519b672a3da097ddddf47
hqvjet <viethq.1906@gmail.com>
2025-01-19 02:19:34 +0700

    update

--- models/BiGRU_CNN_Trans_Enc/__init__.py
+++ models/BiGRU_CNN_Trans_Enc/__init__.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-num_classes = 2
+num_classes = 3
 
 class BiGRU_CNN_Trans_Enc(nn.Module):
-    def __init__(self, input_shape, emb_tech, device, hidden_size=256, gru_layer=2, kerner_size=[1, 2], cnn_filter=128, trans_layer=2, num_head=8, dropout=0.1):
+    def __init__(self, input_shape, emb_tech, device, hidden_size=256, gru_layer=2, kerner_size=[1], cnn_filter=128, trans_layer=2, num_head=8, dropout=0.1):
         super(BiGRU_CNN_Trans_Enc, self).__init__()
         self.model_name = 'BiGRU_CNN_Trans_Enc'
         self.emb_tech = emb_tech
