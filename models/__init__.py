71c66e8eff86614a47f3783fc0e2ecc4fbf0daa7
hqvjet <viethq.1906@gmail.com>
2025-01-19 02:26:05 +0700

    update

--- models/__init__.py
+++ models/__init__.py
@@ -48,8 +48,8 @@ def startTraining(device):
     test_content = np.load(f'res/features/{source}_test_content_features_viso.npy')
 
 
-    train_data = pd.read_csv(f'{DATASET_PATH}/UIT_ViHSD_train_emoji.csv')
-    test_data = pd.read_csv(f'{DATASET_PATH}/UIT_ViHSD_test_emoji.csv')
+    train_data = pd.read_csv(f'{DATASET_PATH}/UIT_VFSD_train_emoji.csv')
+    test_data = pd.read_csv(f'{DATASET_PATH}/UIT_VFSD_test_emoji.csv')
 
     # mapping = {'neg': 0, 'neu': 1, 'pos': 2}
 
