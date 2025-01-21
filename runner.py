ed03a6d2a78a0c1f39980fac3269d4889feea349
hqvjet <viethq.1906@gmail.com>
2025-01-22 02:23:57 +0700

    update

--- runner.py
+++ runner.py
@@ -10,9 +10,15 @@ TRAINING_CMD = ['1\n', '2\n', '3\n', '4\n', '5\n', '6\n', '7\n', '8\n', '9\n']
 def run(command):
     process = subprocess.Popen(['python', '__init__.py'], stdin=subprocess.PIPE, text=True)
     process.communicate(command)
+
+    if process.returncode != 0:
+        print('Process error !')
+        exit()
     
     print('Finish 1 automic process')
-    time.sleep(5)
+    print("Reset GPU...")
+    os.system("kill -9 $(nvidia-smi | grep 'python' | awk '{print $5}')")
+    print('Reset GPU Done !')
 
 print('Do you wanna extract feature ?<y/n>')
 
@@ -34,6 +40,7 @@ if key == 'y':
         for d_c in DATASET_CMD:
             print(f'Extracting Features with extraction model {model} and dataset {d_c}')
             run(m_c + d_c + ACT_CMD[0])
+
             print(f'Finish Extracting Features with extraction model {model} and dataset {d_c}')
         print(f'Finish Extracting Features with extraction model {model}')
 
@@ -49,28 +56,28 @@ for m_c in MODEL_CMD:
     else:
         model = E2V_PHOBERT
 
-    if t_c == '1\n':
-        t_model = BILSTM
-    elif t_c == '2\n':
-        t_model = XGBOOST
-    elif t_c == '3\n':
-        t_model = LR
-    elif t_c == '4\n':
-        t_model = GRU
-    elif t_c == '5\n':
-        t_model = BIGRU
-    elif t_c == '6\n':
-        t_model = CNN
-    elif t_c == '7\n':
-        t_model = ATTENTION_BILSTM
-    elif t_c == '8\n':
-        t_model = CNN_TRANS_ENC
-    else:
-        t_model = BIGRU_CNN_TRANS_ENC
-
     for d_c in DATASET_CMD:
         print(f'Start Training 9 models with dataset {d_c}, extraction model {model}')
         for t_c in TRAINING_CMD:
+            if t_c == '1\n':
+                t_model = BILSTM
+            elif t_c == '2\n':
+                t_model = XGBOOST
+            elif t_c == '3\n':
+                t_model = LR
+            elif t_c == '4\n':
+                t_model = GRU
+            elif t_c == '5\n':
+                t_model = BIGRU
+            elif t_c == '6\n':
+                t_model = CNN
+            elif t_c == '7\n':
+                t_model = ATTENTION_BILSTM
+            elif t_c == '8\n':
+                t_model = CNN_TRANS_ENC
+            else:
+                t_model = BIGRU_CNN_TRANS_ENC
+
             print(f'Training {model}-{d_c}-{t_model}')
             run(m_c + d_c + ACT_CMD[1] + t_c)
             print(f'Finish Training {model}-{d_c}-{t_model}')
