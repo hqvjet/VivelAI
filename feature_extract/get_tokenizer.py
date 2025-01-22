1aadb51215dadec56081bad20f4db8f627aad3b3
hqvjet <viethq.1906@gmail.com>
2025-01-23 00:07:09 +0700

    update

--- feature_extract/get_tokenizer.py
+++ feature_extract/get_tokenizer.py
@@ -4,8 +4,7 @@ from constant import *
 def getTokenizer(extract_model, e_matrix=None):
     tokenizer = AutoTokenizer.from_pretrained(
         PHOBERT_VER if extract_model != VISOBERT else VISOBERT_VER, 
-        use_fast=False if extract_model == VISOBERT else True,
-        force_download=True
+        use_fast=False if extract_model == VISOBERT else True
     )
 
     if e_matrix != None:
