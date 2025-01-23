a9ed35688d8025b084c1c6aae1da21b54f6bb147
hqvjet <viethq.1906@gmail.com>
2025-01-23 14:29:55 +0700

    update

--- mkdir.py
+++ mkdir.py
@@ -2,6 +2,7 @@ import os
 from constant import *
 
 PRE = 'res/models'
+FOLDERS = [AIVIVN, UIT_VIHSD, UIT_VSFC]
 
 if not os.path.exists(PRE + '/' + E2T_PHOBERT):
     os.makedirs(PRE + '/' + E2T_PHOBERT)
@@ -9,11 +10,25 @@ if not os.path.exists(PRE + '/' + E2T_PHOBERT):
 else:
     print('mkdir ' + PRE + '/' + E2T_PHOBERT + ' already exists')
 
+for folder in FOLDERS:
+    if not os.path.exists(PRE + '/' + E2T_PHOBERT + '/' + folder):
+        os.makedirs(PRE + '/' + E2T_PHOBERT + '/' + folder)
+        print('mkdir ' + PRE + '/' + E2T_PHOBERT + '/' + folder)
+    else:
+        print('mkdir ' + PRE + '/' + E2T_PHOBERT + '/' + folder + ' already exists')
+
 if not os.path.exists(PRE + '/' + E2V_PHOBERT):
     os.makedirs(PRE + '/' + E2V_PHOBERT)
     print('mkdir ' + PRE + '/' + E2V_PHOBERT)
 else:
     print('mkdir ' + PRE + '/' + E2V_PHOBERT + ' already exists')
+
+for folder in FOLDERS:
+    if not os.path.exists(PRE + '/' + E2V_PHOBERT + '/' + folder):
+        os.makedirs(PRE + '/' + E2V_PHOBERT + '/' + folder)
+        print('mkdir ' + PRE + '/' + E2V_PHOBERT + '/' + folder)
+    else:
+        print('mkdir ' + PRE + '/' + E2V_PHOBERT + '/' + folder + ' already exists')
 
 if not os.path.exists(PRE + '/' + PHOBERT):
     os.makedirs(PRE + '/' + PHOBERT)
@@ -21,8 +36,22 @@ if not os.path.exists(PRE + '/' + PHOBERT):
 else:
     print('mkdir ' + PRE + '/' + PHOBERT + ' already exists')
 
+for folder in FOLDERS:
+    if not os.path.exists(PRE + '/' + PHOBERT + '/' + folder):
+        os.makedirs(PRE + '/' + PHOBERT + '/' + folder)
+        print('mkdir ' + PRE + '/' + PHOBERT + '/' + folder)
+    else:
+        print('mkdir ' + PRE + '/' + PHOBERT + '/' + folder + ' already exists')
+
 if not os.path.exists(PRE + '/' + VISOBERT):
     os.makedirs(PRE + '/' + VISOBERT)
     print('mkdir ' + PRE + '/' + VISOBERT)
 else:
     print('mkdir ' + PRE + '/' + VISOBERT + ' already exists')
+
+for folder in FOLDERS:
+    if not os.path.exists(PRE + '/' + VISOBERT + '/' + folder):
+        os.makedirs(PRE + '/' + VISOBERT + '/' + folder)
+        print('mkdir ' + PRE + '/' + VISOBERT + '/' + folder)
+    else:
+        print('mkdir ' + PRE + '/' + VISOBERT + '/' + folder + ' already exists')
