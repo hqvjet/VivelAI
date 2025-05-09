bdb5662f7412289ce5de1a066ca6b0ef244de0f3
viethq <viethq.1906@gmail.com>
2025-05-09 07:46:20 +0700

    finish server

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import Title_Comment, Comment
-from input_handler import handle_input_using_E2VPhoBERT
-from constant import *
+from input_handler import handle_input_using_E2VPhoBERT, handle_input_using_E2TPhoBERT
 
 app = FastAPI()
 
 # # Set up CORS
-# allowed_origins = ['http://localhost:4567']
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[allowed_origins],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+allowed_origins = ['http://localhost:3000']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/api/v1/E2V-PhoBERT/predict")
 def read_root(req: Comment):
-    ext_model = E2V_PHOBERT
-    res = handle_input_using_E2VPhoBERT(req, extract_model=ext_model)[0]
+    res = handle_input_using_E2VPhoBERT(req)[0]
+    if res == 0:
+        res = 'Negative'
+    else:
+        res = 'Positive'
+
+    return {'Sentiment': res}
+
+@app.post("/api/v1/VED-PhoBERT/predict")
+def read_root(req: Comment):
+    res = handle_input_using_E2TPhoBERT(req)[0]
     if res == 0:
         res = 'Negative'
     else:
--- input_handler.py
+++ input_handler.py
@@ -14,6 +14,7 @@ from feature_extract.get_tokenizer import getTokenizer
 from constant import *
 
 from models.BiGRU import BiGRU
+from models.GRU import GRU
 
 if torch.cuda.is_available():
     print('USING GPU')
@@ -22,29 +23,51 @@ else:
     print('USING CPU')
     device = torch.device('cpu')
     
-title_size = torch.Size((1, 6144))
 emoji_size = torch.Size((1, 3072))
 
-emoji_model = BiGRU(device=device, input_shape=emoji_size, num_classes=2)
-emoji_model = emoji_model.to(device)
-emoji_model.load_state_dict(torch.load('res/models/E2V-PHOBERT/AIVIVN/BiGRU.pth'))
-emoji_model.eval()
-e_matrix = getEmojiEmbeddingMatrix()
-tokenizer = getTokenizer(E2V_PHOBERT, e_matrix)
+E2V_model = BiGRU(device=device, input_shape=emoji_size, num_classes=2)
+E2V_model = E2V_model.to(device)
+E2V_model.load_state_dict(torch.load('res/models/E2V-PHOBERT/AIVIVN/BiGRU.pth'))
+E2V_model.eval()
+E2T_model = GRU(device=device, input_shape=emoji_size, num_classes=2)
+E2T_model = E2T_model.to(device)
+E2T_model.load_state_dict(torch.load('res/models/E2T-PHOBERT/AIVIVN/GRU.pth'))
+E2T_model.eval()
 
-def handle_input_using_E2VPhoBERT(input: Comment, extract_model=None):
+e_matrix = getEmojiEmbeddingMatrix()
+E2V_tokenizer = getTokenizer(E2V_PHOBERT, e_matrix)
+E2T_tokenizer = getTokenizer(E2T_PHOBERT)
+
+def handle_input_using_E2VPhoBERT(input: Comment):
     comment = pd.Series([input.comment])
 
-    comment = useNormalize(comment, extract_model)
+    comment = useNormalize(comment, E2V_PHOBERT)
     comment = useLemma(comment)
     comment = useTokenize(comment)
     comment = removeStopword(comment)
     comment = emojiHandling(comment, mode='unicode') 
-    comment, comment_attention = useIdentify(comment, tokenizer)
-    comment = extractFeature(device, comment, comment_attention, extract_model=extract_model, tokenizer=tokenizer, emoji_matrix=e_matrix)
+    comment, comment_attention = useIdentify(comment, E2V_tokenizer)
+    comment = extractFeature(device, comment, comment_attention, extract_model=E2V_PHOBERT, tokenizer=E2V_tokenizer, emoji_matrix=e_matrix)
 
     inp = comment.to(device)
-    pred = emoji_model(inp)
+    pred = E2V_model(inp)
+    _, pred = torch.max(pred.data, 1)
+
+    return pred.cpu().tolist()
+
+def handle_input_using_E2TPhoBERT(input: Comment):
+    comment = pd.Series([input.comment])
+
+    comment = useNormalize(comment, E2T_PHOBERT)
+    comment = useLemma(comment)
+    comment = useTokenize(comment)
+    comment = removeStopword(comment)
+    comment = emojiHandling(comment, mode='text') 
+    comment, comment_attention = useIdentify(comment, E2T_tokenizer)
+    comment = extractFeature(device, comment, comment_attention, extract_model=E2T_PHOBERT, tokenizer=E2T_tokenizer, emoji_matrix=e_matrix)
+
+    inp = comment.to(device)
+    pred = E2T_model(inp)
     _, pred = torch.max(pred.data, 1)
 
     return pred.cpu().tolist()
